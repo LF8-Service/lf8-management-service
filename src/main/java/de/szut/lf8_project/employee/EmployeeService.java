61e78e05240213074407a7758e57b21c7e9eec50
VladSeit <firelink012@gmail.com>
2023-11-01 10:50:51 +0100

    Access token als Methode

--- src/main/java/de/szut/lf8_project/employee/EmployeeService.java
+++ src/main/java/de/szut/lf8_project/employee/EmployeeService.java
@@ -1,33 +1,30 @@
 package de.szut.lf8_project.employee;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class EmployeeService {
 
-    public String makeGetRequest(String URL) {
+    public String makeGetRequest(String serverUrl) {
         try {
-            // Укажите URL-адрес сервера, к которому вы хотите отправить запрос.
-            String serverUrl = URL;
 
-            // Укажите свой Bearer Token.
-            String bearerToken = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE2OTcxMDE4OTUsImlhdCI6MTY5NzA5ODI5NSwianRpIjoiM2RkNWVmN2QtNjg4OS00YjJiLTg0MWQtNGUxNjFiZTg5MDc3IiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiIyNjI4MTQ1OS01ODk1LTQyMTAtYWZhNy01ZWYzZmU4ZDEwMTgiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.A8KnKJ3FxVgosnFg6pjWXYxEGJnpiY2ivg9ycSeGiHZeezTpBRicqgWtAsQegcjCVAQwWksy-a_gtLvr0Gfc3f40QQ9r-MO1gX2c4KyfvHZuDzhMKG6M_jcBvAeYz1qilfctoYH3ouLUYVnQpvZgUkqnNI9usMXex_b43cmVGaXirBgNt9DSqwsgR_Tna_ksVeT1Vpxj2w103reuwFScPS_Z9lPWmLl7o6xolgbnt6I7Bi3aWEH84czGFYuVGHhpEr56FL_LPzCueJQnUK8YQzamDhqM22XpfvhU2jgiS9EE37OEp_iSwxRzQs4lEHozDuv6tvAEl1NRvIfIIpFCyw";
+            String bearerToken = getAccessToken();
 
-            // Создайте объект URL и откройте соединение.
             URL url = new URL(serverUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            // Укажите метод запроса (GET, POST, PUT, DELETE и т. д.).
             connection.setRequestMethod("GET");
 
-            // Добавьте заголовок "Authorization" с Bearer Token.
             connection.setRequestProperty("Authorization", "Bearer " + bearerToken);
 
-            // Получите ответ от сервера.
             int responseCode = connection.getResponseCode();
             System.out.println("Response Code: " + responseCode);
 
@@ -42,7 +39,6 @@ public class EmployeeService {
 
                 in.close();
                 connection.disconnect();
-                // Выведите ответ от сервера.
                 return response.toString();
             } else {
                 return "404";
@@ -51,5 +47,52 @@ public class EmployeeService {
         } catch (Exception e) {
          return e.toString();
         }
+    }
+    private String getAccessToken() {
+        try {
+            String url = "https://keycloak.szut.dev/auth/realms/szut/protocol/openid-connect/token";
+            String clientId = "employee-management-service";
+            String username = "user";
+            String password = "test";
+
+            String requestBody = "grant_type=password&client_id=" + clientId + "&username=" + username + "&password=" + password;
+
+            URL obj = new URL(url);
+
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+            connection.setRequestMethod("POST");
+
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            connection.setDoOutput(true);
+
+            try (DataOutputStream out = new DataOutputStream(connection.getOutputStream())) {
+                out.write(requestBody.getBytes(StandardCharsets.UTF_8));
+            }
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                connection.disconnect();
+                JSONObject jsonObject = new JSONObject(response.toString());
+
+                String accessToken = jsonObject.getString("access_token");
+                return accessToken;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "404";
+    }
+
+    public static void main(String[] args) {
+        EmployeeService em= new EmployeeService();
+       System.out.println(em.getAccessToken());
     }
 }
